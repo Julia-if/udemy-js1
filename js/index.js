07270fa7720b45978b878899c4a44fd1bcf4a231
Yuliia <juliaif18@gmail.com>
2022-12-07 00:05:42 +0200

    task1

--- js/index.js
+++ js/index.js
@@ -1,11 +1,15 @@
-
-
-
-let incr=10,
-decr=10;
-
-++incr;
---decr ;
-
-console.log(incr);
-console.log(decr);
+const numberOfFilms = +prompt("How many movies did you watch?","");
+ const personalMovieDB ={
+    count: numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat: false,
+ };
+ const lastMovie = prompt('Last movie?','');
+ const level =prompt('Level?','');
+ const lastMovieL = prompt('Last movie?','');
+ const levelL =prompt('Level?','');
+ personalMovieDB.movies[lastMovie]=level;
+ personalMovieDB.movies[lastMovieL]=levelL;
+ console.log(personalMovieDB);
